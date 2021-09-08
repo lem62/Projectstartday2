800db19127883160b2350049507294774851a980
lhankel <lhankel@student.bridgew.edu>
2021-09-08 14:43:20 -0400

    Update 9/8/21

---
+++ src/com/company/day2main2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class day2main2 {
+    public static void main(String[]args) {
+        //Type code below
+
+    }
+
+}
---
+++ src/example3.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class example3 {
+    public static void main(String[]args) {
+
+    }
+}
