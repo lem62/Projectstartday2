8a492bee215e47c79bf863734791e7ed864dee57
lhankel <lhankel@student.bridgew.edu>
2021-09-13 14:02:34 -0400

    Update 9/13/21

--- src/example3.java
+++ src/example3.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class example3 {
     public static void main(String[]args) {
     // type code below
         var studentsNames = new ArrayList<String>();
         var reader = new Scanner(System.in);
         var userResponse = "";
-        while (!userResponse.equals("done")){
+        while (!userResponse.equals("done")) {
             System.out.print("Type in the next student name or 'done' to exit:");
             userResponse = reader.nextLine();
-            if (userResponse.equals("done")){
+            if (userResponse.equals("done")) {
                 break;
+
             }
             studentsNames.add(userResponse);
+        }
+        System.out.println("All of your students in the class below:");
+        for (var studentname : studentsNames){
+            System.out.println(studentname);
+
         }
     }
 }
